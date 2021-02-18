6576fe146c82d3a1ef56df67315da27293d8e755
デンパ時計 <jsoldenpa@yahoo.co.jp>
2021-02-18 22:41:26 +0900

    プロジェクト ファイルを追加します。

---
+++ DisableUnityGC/DisableUnityGCController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Scripting;
+
+namespace DisableUnityGC
+{
+    /// <summary>
+    /// Monobehaviours (scripts) are added to GameObjects.
+    /// For a full list of Messages a Monobehaviour can receive from the game, see https://docs.unity3d.com/ScriptReference/MonoBehaviour.html.
+    /// </summary>
+    public class DisableUnityGCController : MonoBehaviour
+    {
+        // These methods are automatically called by Unity, you should remove any you aren't using.
+        #region Monobehaviour Messages
+        /// <summary>
+        /// Only ever called once, mainly used to initialize variables.
+        /// </summary>
+        private void Awake()
+        {
+            Plugin.Log?.Debug($"{name}: Awake()");
+            SceneManager.activeSceneChanged += this.SceneManager_activeSceneChanged;
+        }
+
+        /// <summary>
+        /// Called when the script is being destroyed.
+        /// </summary>
+        private void OnDestroy()
+        {
+            Plugin.Log?.Debug($"{name}: OnDestroy()");
+            SceneManager.activeSceneChanged -= this.SceneManager_activeSceneChanged;
+        }
+        #endregion
+
+        private void SceneManager_activeSceneChanged(Scene arg0, Scene arg1)
+        {
+            if (arg1.name == "MenuCore") {
+                GarbageCollector.GCMode = GarbageCollector.Mode.Enabled;
+                GC.Collect();
+            }
+        }
+    }
+}
---
+++ DisableUnityGC/Installer/DisableGCAppInstaller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zenject;
+using SiraUtil;
+
+namespace DisableUnityGC.Installer
+{
+    public class DisableGCAppInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            this.Container.BindInterfacesAndSelfTo<DisableUnityGCController>().FromNewComponentOnNewGameObject(nameof(DisableUnityGCController)).AsSingle().NonLazy();
+        }
+    }
+}
